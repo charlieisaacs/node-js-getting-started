77f0ada8e5c2f3c351b1bfdade4b2a51a68b4f40
Charlie Isaacs <cisaacs@salesforce.com>
2021-12-05 22:38:13 -0500

    test

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ var http = require("https");
 var bodyParser = require('body-parser');
 var deviceId="";
 
+ //sourceDeviceId: 4525,
+ //userId: 1485,
+//occurrenceTime: 1638761339627,
+//tenantId: '507c100c-7242-4818-bf87-30f7786e61d6',
+//eventType: 'com.anyconnect.event.vision',
+ //snapshotUrl: 'https://accind0ds1.blob.core.windows.net/eventrecording0/4525/000001638761339627_snapshot.jpg?sig=J7bbesUaAyklK5nVc6s%2Bfio0FP4vKlfays1aGF6346I%3D&se=2021-12-06T04%3A29%3A21Z&sv=2019-02-02&sp=rcw&sr=b',
+ //sourceImagerDetails: 'In Cabin Camera',
+ //eventTriggerDetails: 'No Driver & Camera Occlusion',
+//summary: ''
+sourceDeviceId= "";
+userId= "";
+occurrenceTime= "";
+tenantId= "";
+eventType= "";
+snapshotUrl= "";
+eventTriggerDetails= "";
+summary= "";
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
  
@@ -55,6 +73,16 @@ app.post('/smarterai', function (req1, res1) {
     res.on("end", function (chunk) {
       var body = Buffer.concat(chunks);
       console.log(body.toString());
+      sourceDeviceId=body.sourceDeviceId;
+      console.log("HERE IS sourceDeviceId"+sourceDeviceId);
+ //userId= "";1485,
+//occurrenceTime= "";1638761339627,
+//tenantId= "";'507c100c-7242-4818-bf87-30f7786e61d6',
+//eventType= "";'com.anyconnect.event.vision',
+// snapshotUrl= "";'https:accind0ds1.blob.core.windows.net/eventrecording0/4525/000001638761339627_snapshot.jpg?sig=J7bbesUaAyklK5nVc6s%2Bfio0FP4vKlfays1aGF6346I%3D&se=2021-12-06T04%3A29%3A21Z&sv=2019-02-02&sp=rcw&sr=b',
+// sourceImagerDetails= "";'In Cabin Camera',
+// eventTriggerDetails= "";'No Driver & Camera Occlusion',
+//summary= "";'
     });
   
     res.on("error", function (error) {
